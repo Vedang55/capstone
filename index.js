72d45dbcf8e5e6563bc80f68997512b4e3f67e54
Vedang55 <Vedang55.vd@gmail.com>
2019-10-17 19:41:34 +0530

    ui functional changes

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ a.addEventListener('click',buttonclk)
 b.addEventListener('click',buttonclk)
 var inputField = document.querySelector('#inputField');
 var answerField = document.getElementById("answerfield")
+var operators = ['x','-','+','/'];
 
 
 function buttonclk(event){
         var elementClicked = event.target.innerHTML;
         if(elementClicked != 'DEL' && elementClicked != '=' ){
-          inputField.value = inputField.value + elementClicked; 
-          updateField();
+        
+            if(operators.includes(elementClicked) && operators.includes(inputField.value[inputField.value.length - 1])){
+                inputField.value = inputField.value.substring(0, inputField.value.length - 1) + elementClicked;
+            }
+            else{
+                inputField.value = inputField.value + elementClicked; 
+            }
+            updateField();
         }
         else if(elementClicked === 'DEL') {
             if(inputField.value.length > 1){
@@ -33,12 +40,21 @@ function buttonclk(event){
 
 function updateField(){  
     try{
-        answerField.innerHTML = eval(inputField.value.replace('x','*'));
+        answerField.innerHTML = eval(inputField.value.replace(/x/g,'*'));
+        if(answerField.innerHTML != inputField.value){
+            
+        }
+        else{
+            answerField.innerHTML = "";
+        }
     }
     catch(e){
-
+        if(eval(inputField.value.substring(0, inputField.value.length - 1).replace(/x/g,'*')) == inputField.value.substring(0, inputField.value.length - 1)){
+            answerField.innerHTML = "";
+        }
     }
 
 }
+
 
 
